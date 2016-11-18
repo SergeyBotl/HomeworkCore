e751b99bfead56768512b9756b11b7f9031b75f6
Sergey <bot74@mail.ru>
2016-11-18 09:16:24 +0200

    commit

--- src/module09/Test.java
+++ src/module09/Test.java
@@ -1,10 +1,26 @@
 package module09;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
 import java.util.stream.Stream;
 
 public class Test {
     public static void main(String[] args) {
-        Stream.of("dd2", "aa2", "bb1", "bb3", "cc4")
+        String ss="rr";
+
+
+        List<String> mList = Arrays.asList("aa1","cc2", "cc1", "aa2", "bb1");
+
+        mList
+                .stream()
+                .filter(s -> s.startsWith("a"))
+                //.map(String::toUpperCase)
+                .sorted()
+                .forEach(System.out::println);
+
+
+       /* Stream.of("dd2", "aa2", "bb1", "bb3", "cc4")
                 .map(s -> {
                     System.out.println("map: " + s);
                     return s.toUpperCase();
@@ -12,7 +28,7 @@ public class Test {
                 .anyMatch(s -> {
                     System.out.println("anyMatch: " + s);
                     return s.startsWith("A");
-                });
+                });*/
     }
 
 
